928d91a12112309458097cb688d3f48ac55e4b44
Febrilian Kristiawan <febriliankr@gmail.com>
2022-07-12 17:51:01 +0700

    added w3 and w4

--- main.go
+++ main.go
@@ -17,5 +17,7 @@ func main() {
 	}
 	log.Println("Attempting to start server on port " + port)
 	e.Static("/", "public")
+	e.Static("/w3", "w3")
+	e.Static("/w4", "w4")
 	e.Logger.Fatal(e.Start(":" + port))
 }
